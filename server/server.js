616535ccb7dc71a453c05ec861f8c560b9512f8a
Santiago Pintus <santiagopintus@gmail.com>
2023-04-05 19:13:15 -0300

    Server finished. Edited the edit form a little. Removed .env file. Connected to my web-service published on Render

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-// Get dependencies
 const express = require("express");
 const path = require("path");
 const http = require("http");
@@ -7,46 +6,31 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const db = require("./db");
 const cors = require("cors");
+const routes = require("./routes");
 
-// ... ADD CODE TO IMPORT YOUR ROUTING FILES HERE ...
+const getDb = () => {
+  return db;
+};
 
-const app = express(); // create an instance of express
+const createServer = (port = process.env.PORT || 3000) => {
+  const app = express();
 
-// Tell express to use the following parsers for POST data
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: false,
-  })
-);
-app.use(cookieParser());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(logger("dev"));
+  app.use(cors());
+  app.use(express.static(path.join(__dirname, "dist/cms")));
 
-app.use(logger("dev")); // Tell express to use the Morgan logger
+  app.use("/", routes);
 
-app.use(cors());
+  app.get("*", (req, res) => {
+    res.redirect("/");
+  });
 
-// Tell express to use the specified director as the
-// root directory for your web site
-app.use(express.static(path.join(__dirname, "dist/cms")));
+  app.set("port", port);
 
-// Tell express to map the default route ('/') to the index route
-app.use("/", require("./routes"));
+  return http.createServer(app);
+};
 
-// ... ADD YOUR CODE TO MAP YOUR URL'S TO ROUTING FILES HERE ...
-
-// Tell express to map all other non-defined routes back to the index page
-app.get("*", (req, res) => {
-  res.redirect("/");
-});
-
-// Define the port address and tell express to use this port
-const port = process.env.PORT || "3000";
-app.set("port", port);
-
-// Create HTTP server.
-const server = http.createServer(app);
-
-// Tell the server to start listening on the provided port
-server.listen(port, () => {
-  console.log("API running on localhost: " + port);
-});
+module.exports = createServer;
